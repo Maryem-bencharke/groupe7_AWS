25e61d36d26fbd435f6738fc4fa5a54a9f2a2986
yassir nouri <yassirnouri00@gmail.com>
2025-03-25 05:33:14 +0100

    petit correction

--- pendu.js
+++ pendu.js
@@ -1,5 +1,5 @@
-//var socket = io('https://groupe7-aws.onrender.com');
-const socket = io("http://127.0.0.1:3000");
+//const socket = io("http://127.0.0.1:3000");
+var socket = io('https://groupe7-aws.onrender.com');
 let life = 6;
 let wordToGuess = "";
 let lettersTyped = [];
